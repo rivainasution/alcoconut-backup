b09c1932c7a92ffa42cf633e3a1babb52811f673
rivainasution <rivai_nasution@student.ub.ac.id>
2022-09-13 10:12:56 +0700

    First Commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 function Footers(){
     return (
         <div className="bg-dark p-3">
-            <h6 className="text-center text-light">&copy; 2022 Sipirol Coffee. All rights reserved.</h6>
+            <h6 className="text-center text-light">&copy; 2022 Sipirok Coffee. All rights reserved.</h6>
         </div>
     );
 }
